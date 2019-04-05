5c0cb637ec8dc56e3ebcdbe35aeb3ebb10077191
guiaixiao <824541050@qq.com>
2019-04-05 15:40:53 +0800

    refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest
 from django.test import TestCase
+from django.template.loader import render_to_string
 from django.urls import resolve
 
 from lists.views import home_page
@@ -9,15 +10,36 @@ from lists.views import home_page
     # def test_bad_maths(self):
         # self.assertEqual(1 + 1,3)
 class HomePageTest(TestCase):
+# 
+    # def test_root_url_resolves_to_home_page_view(self):
+        # found = resolve('/')
+        # self.assertEqual(found.func,home_page)
+    def test_uses_tome_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func,home_page)
 
-    def test_home_page_return_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+
         html = response.content.decode('utf-8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>',html)
         self.assertTrue(html.endswith('</html>'))
+
+        self.assertTemplateUsed(response,'home.html')
+    # def test_home_page_return_correct_html(self):
+        # request = HttpRequest()
+        # response = home_page(request)
+        # html = response.content.decode('utf-8')
+        # self.assertTrue(html.startswith('<html>'))
+        # self.assertIn('<title>To-Do lists</title>',html)
+        # self.assertTrue(html.endswith('</html>'))
+
+
+    # def test_home_page_returns_correct_html(self):  
+        # request = HttpRequest() 
+        # response = home_page(request)   
+        # html = response.content.decode('utf-8') 
+        # expected_html = render_to_string('home.html')   
+        # self.assertEqual(html,expected_html)    
