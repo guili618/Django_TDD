b0a4bc551d3415fda2545ae64f58213baaedf06b
guiaixiao <824541050@qq.com>
2019-04-05 14:49:50 +0800

    basic view now return minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -13,3 +13,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func,home_page)
+
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>',html)
+        self.assertTrue(html.endswith('</html>'))
