9ac970d32b53dd432bcf3f77f75fa2e060ffd0ff
guiaixiao <824541050@qq.com>
2019-04-06 18:42:15 +0800

    http redirect

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from .models import Item
 
 
 # Create your views here.
 
 def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        #new_item_text = request.POST['item_text']
+        return redirect('/')
+    #else:
+        new_item_text = ""       
     #if request.method == 'POST':
     #    return HttpResponse(request.POST['item_text'])
-    return render(request,'home.html',{'new_item_text':request.POST.get('item_text','')})
+    return render(request,'home.html')
